488e2180631db36a985a31340c64324423b5dd56
umsimplesrodrigo <umsimplesti@gmail.com>
2025-04-10 13:11:30 -0300

    Spring Boot Inicializado

---
+++ comunicacao-bot-bling/src/test/java/com/umsimplesrodrigo/comunicacao_bot_bling/ComunicacaoBotBlingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.umsimplesrodrigo.comunicacao_bot_bling;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ComunicacaoBotBlingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
